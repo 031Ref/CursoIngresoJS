8bd72ebdcccebb949085b75c9a891a8a4193faa0
alumno <alumno>
2017-02-13 10:15:39 -0300

    Termine los ejercicios de switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,26 @@
 function Mostrar()
 {
-//tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
-
-}//FIN DE LA FUNCIÓN
+switch(mesDelAño){
+	case "Enero":
+	case "Febrero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	alert(mesDelAño+": Falta para el invierno.");
+	break;
+	case "Julio":
+	case "Agosto":
+	alert(mesDelAño+": Abrigate que hace frio.");
+	break;
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	alert(mesDelAño+": Ya pasamos el frio, ahora calor!!!.");
+	break;
+	default:
+	break;
+}
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,24 @@
 function Mostrar()
 {
-//tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
-	
-	
-
-
-}//FIN DE LA FUNCIÓN
+switch(mesDelAño){
+	case "Enero":
+	case "Marzo":
+	case "Abril":
+	case "Mayo":
+	case "Junio":
+	case "Julio":
+	case "Agosto":
+	case "Septiembre":
+	case "Octubre":
+	case "Noviembre":
+	case "Diciembre":
+	alert(mesDelAño+": Este mes tiene 30 o más días");
+	break;
+	case "Febrero":
+	alert(mesDelAño+":  Este mes no tiene más de 29 días.");
+	break;
+	default:
+	break;
+}
+}
