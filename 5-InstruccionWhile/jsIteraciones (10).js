60f8ee654a653817e2ae6c57c332023192acc0eb
alumno <alumno>
2017-02-20 12:20:23 -0300

    extra

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,5 +1,94 @@
 function Mostrar()
 {
+	//mi manera
+/*
+	var nota;
+	var aprovados=0;
+	var notasProm=0;
+	var cont=0;
+	while(confirm("ingrese nota")){
+		nota=prompt("");
+		nota=parseInt(nota);
+		if(nota>=1&&nota<=10) {
+			if (nota>=4) {
+				aprovados++;
+			}
+			cont++;
+			notasProm+=nota;
+		}
+	}
+	console.log("aprovados "+aprovados,"promedio de notas "+(notasProm/cont));
+*/
+	//la del profe
+	var nota=-1;
+	var acumNota=0;
+	var contNotas=0;
+	var contNT=0;
+	var primera=true;
+	var primeraF=true;
+	var primeraM=true;
+	var notaMax;
+	var notaMin;
+	var notaMaxF;
+	var notaMinF;
+	var notaMaxM;
+	var notaMinM;
+	var sexo;
+	while(confirm("")){
+
+		while(nota<1||nota>10){
+			console.log(nota);
+			nota=prompt("");
+			nota=parseInt(nota);
+		}
+		while(sexo!="f"&&sexo!="m"){
+			sexo=prompt("")
+		}
+		contNT++;
+		if (nota>=4) {
+			acumNota+=nota;
+			contNotas++;
+		}
+		if (primera) {
+			primera=false;
+			notaMax=nota;
+			notaMin=nota;
+		}else{
+			if (nota>notaMax) {
+				notaMax=nota;
+			}
+			if (nota<notaMin) {
+				notaMin=nota;
+			}
+		}
+		if (primeraF) {
+			primeraF=false;
+			notaMaxF=nota;
+			notaMinF=nota;
+		}else{
+			if (nota>notaMaxF) {
+				notaMaxF=nota;
+			}
+			if (nota<notaMinF) {
+				notaMinF=nota;
+			}
+		}
+		if (primeraM) {
+			primeraM=false;
+			notaMaxM=nota;
+			notaMinM=nota;
+		}else{
+			if (nota>notaMaxM) {
+				notaMaxM=nota;
+			}
+			if (nota<notaMinM) {
+				notaMinM=nota;
+			}
+		}
+		nota=-1;
+		sexo="k";
+	}
+/*
 	var num;
 	var pos=0;
 	var neg=0;
@@ -31,4 +120,5 @@ function Mostrar()
 		dif=pos-neg;
 	}
 	console.log("positivos "+posCant,"negativos "+negCant,"ceros "+cerCant,"promedio positivos "+posProm,"promedio negativos "+negProm,"diferencia "+dif,"pares "+parCant)
+*/
 }
