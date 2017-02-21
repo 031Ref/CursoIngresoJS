d23e078b4e304b0e5193f9c3e394f771ad51bbef
alumno <alumno>
2017-02-21 11:30:24 -0300

    agrege contador de rachas

--- 8-TPs/jsPiedarPapelTijera1.js
+++ 8-TPs/jsPiedarPapelTijera1.js
@@ -5,7 +5,9 @@ El jugador seleccionará una imagen correspondiente
 a su opción  y le informaremos si ganó, empató o perdió.
 */
 var eleccionMaquina;
-
+var rachaV=0;
+var rachaP=0;
+var rachaE=0;
 function comenzar()
 {
 	eleccionMaquina=Math.floor((Math.random() * 3) + 1);
@@ -14,31 +16,61 @@ function piedra()
 {
 	if (eleccionMaquina==1) {
 		alert("Usted empató");
+		rachaE++;
+		rachaP=0;
+		rachaV=0;
 	}else if (eleccionMaquina==2) {
 		alert("Usted perdió");
+		rachaP++;
+		rachaE=0;
+		rachaV=0;
 	}else if (eleccionMaquina==3) {
 		alert("Usted ganó");
+		rachaV++;
+		rachaP=0;
+		rachaE=0;
 	}
+	console.log(rachaV,rachaE,rachaP);
 }
 function papel()
 {
 
 	if (eleccionMaquina==2) {
 		alert("Usted empató");
+		rachaE++;
+		rachaP=0;
+		rachaV=0;
 	}else if (eleccionMaquina==3) {
 		alert("Usted perdió");
+		rachaP++;
+		rachaE=0;
+		rachaV=0;
 	}else if (eleccionMaquina==1) {
 		alert("Usted ganó");
+		rachaV++;
+		rachaP=0;
+		rachaE=0;
 	}
+	console.log(rachaV,rachaE,rachaP);
 }
 function tijera()
 {
 	
 	if (eleccionMaquina==3) {
 		alert("Usted empató");
+		rachaE++;
+		rachaP=0;
+		rachaV=0;
 	}else if (eleccionMaquina==1) {
 		alert("Usted perdió");
+		rachaP++;
+		rachaE=0;
+		rachaV=0;
 	}else if (eleccionMaquina==2) {
 		alert("Usted ganó");
+		rachaV++;
+		rachaP=0;
+		rachaE=0;
 	}
+	console.log(rachaV,rachaE,rachaP);
 }
