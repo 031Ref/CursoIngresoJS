46c7b73745c9e2f7c1af5e66d31dbb42f96b5829
alumno <alumno>
2017-02-21 09:17:38 -0300

    termine el segundo adivina el numero

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,18 +11,51 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
+var sNum;
 
 function comenzar()
 {
-	//Genero el número RANDOM entre 1 y 100
-	 
-	
-
+	sNum=Math.floor((Math.random() * 100) + 1);
+	contadorIntentos=0;
+	console.log(sNum);
 }
 
 function verificar()
 {
-	
-	
-
+	contadorIntentos++;
+	var mNum=document.getElementById('numero').value;
+	document.getElementById('intentos').value=contadorIntentos;
+	if (mNum==sNum) {
+		switch(contadorIntentos){
+			case 1:
+			alert("usted es un Psíquico");
+			break;
+			case 2:
+			alert("excelente percepción");
+			break;
+			case 3:
+			alert("Esto es suerte");
+			break;
+			case 4:
+			alert("Excelente técnica");
+			break;
+			case 5:
+			alert("usted está en la media");
+			break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+			alert("falta técnica");
+			break;
+			default:
+			alert("afortunado en el amor!!");
+			break;
+		}
+	}else if (mNum<sNum) {
+		alert("te falta para el numero");
+	}else if (mNum>sNum) {
+		alert("te pasate del numero");
+	}
 }
