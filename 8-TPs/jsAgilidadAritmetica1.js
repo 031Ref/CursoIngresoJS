5f50a0c5a54d7d35e50e0bc700c4834de9f7eebb
alumno <alumno>
2017-02-21 09:53:34 -0300

    complete aritmetica uno

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,14 +7,55 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var sNum1;
+var sNum2;
+var sNum3;
+var result;
 function comenzar()
 {
-
-	
-
-}//FIN DE LA FUNCIÓN
+	sNum1=Math.floor((Math.random()*10)+1);
+	console.log(sNum1);
+	document.getElementById('PrimerNumero').value=sNum1;
+	sNum2=Math.floor((Math.random()*10)+1);
+	console.log(sNum2);
+	document.getElementById('SegundoNumero').value=sNum2;
+	sNum3=Math.floor((Math.random()*4)+1);
+	console.log(sNum3);
+	switch(sNum3){
+		case 1:
+		document.getElementById('Operador').value="+";
+		break;
+		case 2:
+		document.getElementById('Operador').value="-";
+		break;
+		case 3:
+		document.getElementById('Operador').value="*";
+		break;
+		case 4:
+		document.getElementById('Operador').value="/";
+		break;
+	}
+}
 function Responder()
 {
-	
-
-}//FIN DE LA FUNCIÓN
+	respuesta=document.getElementById('Respuesta').value;
+	switch(sNum3){
+		case 1:
+		result=sNum1+sNum2;
+		break;
+		case 2:
+		result=sNum1-sNum2;
+		break;
+		case 3:
+		result=sNum1*sNum2;
+		break;
+		case 4:
+		result=sNum1/sNum2;
+		break;
+	}
+	if (respuesta==result) {
+		alert("Respuesta correcta");
+	}else{
+		alert("Respuesta iconrrecta");
+	}
+}
