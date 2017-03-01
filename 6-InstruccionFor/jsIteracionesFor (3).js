9016418eb3a6c0ffb309b6635a597b800a27ff66
alumno <alumno>
2017-03-01 10:19:50 -0300

    terminados ejercicios for

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,7 @@
 function Mostrar()
 {
-
-var repetciones = prompt("ingrese el número de repeticiones");
-
-
-
-}//FIN DE LA FUNCIÓN
+	var repe = prompt("ingrese el número de repeticiones");
+	for (var i = 0; i<repe; i++) {
+		console.log("Hola UTN FRA");
+	}
+}
